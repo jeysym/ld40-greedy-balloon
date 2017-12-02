795e3c2e1e4f86a83f1931baf0de6beeaa2b2058
Jeysym <bryda.jan@gmail.com>
2017-12-02 11:10:44 +0100

    Add goldBarCount to Balloon and gold bar slowdown. And GoldBar prefab.

--- LD40/Assets/Scripts/BalloonController.cs
+++ LD40/Assets/Scripts/BalloonController.cs
@@ -6,8 +6,13 @@ public class BalloonController : MonoBehaviour {
 
 	private Rigidbody2D rb;
 
-	public float hSpeed = 1.0f;
-	public float vSpeed = 1.0f;
+    public int goldBarCount = 0;
+
+    public float goldenBarSlowdown; 
+	public float hSpeed;
+	public float vSpeed;
+
+    private bool xWasDown = false;
 
 	// Use this for initialization
 	void Start () {
@@ -18,11 +23,34 @@ public class BalloonController : MonoBehaviour {
 	void Update () {
 		float hAxis = Input.GetAxis("Horizontal");
 		bool spaceDown = Input.GetKey(KeyCode.Space);
+        bool xDown = Input.GetKey(KeyCode.X);
+
+        float maxVForce = vSpeed + (1 - goldenBarSlowdown) * (float)goldBarCount;
+        float maxHForce = hSpeed * hAxis;
 
 		if (spaceDown)
 		{
-			rb.AddForce(new Vector2(0.0f, 1.0f) * vSpeed);
+			rb.AddForce(new Vector2(0.0f, 1.0f) * maxVForce);
 		}
-		rb.AddForce(new Vector2(1.0f, 0.0f) * hAxis * hSpeed);
+		rb.AddForce(new Vector2(1.0f, 0.0f) * maxHForce);
+
+        if (xDown && xWasDown == false)
+        {
+            xWasDown = true;
+            if (goldBarCount > 0)
+                goldBarCount--;
+        }
+        
+        if (xDown == false)
+            xWasDown = false;
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("GoldenBar"))
+        {
+            other.gameObject.SetActive(false);
+            goldBarCount++;
+        }
+    }
 }
