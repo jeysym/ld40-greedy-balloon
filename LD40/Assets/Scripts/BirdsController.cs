b47ce1cfae10cc0d687cca29f8e0156eeebf6450
maoap1 <maoap1@gmail.com>
2017-12-02 15:50:12 +0100

    Birds

---
+++ LD40/Assets/Scripts/BirdsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsController : MonoBehaviour {
+
+	public float Bottom;
+	public float Top;
+	public float RightEdge;
+	public float SpawnTime;
+	public GameObject Bird;
+	public GameObject Balloon;
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", SpawnTime, SpawnTime);
+	}
+
+
+	void Spawn()
+	{
+		float Placement = Random.Range(Bottom,Top);
+		Bird = Instantiate(Bird,new Vector3(Balloon.transform.position.x+2*RightEdge,Placement),Quaternion.identity);
+	}
+
+}
