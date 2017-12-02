f9b0d08236034dcdcc426ccaf7be68554575c1dc
Jeysym <bryda.jan@gmail.com>
2017-12-02 12:54:58 +0100

    Add UI elements to game.

---
+++ LD40/Assets/Scripts/FloatingMotion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingMotion : MonoBehaviour {
+
+    public float Amplitude;
+    public float Freq;
+
+    private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start () {
+        originalPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void FixedUpdate()
+    {
+        float yOffset = Amplitude * Mathf.Sin(Time.time * Freq);
+        transform.position = new Vector3(originalPosition.x, originalPosition.y + yOffset, originalPosition.z);
+    }
+}
