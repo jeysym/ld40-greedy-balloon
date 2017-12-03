a16def91c56f42bc3c4cc73afa5b49a549ff1a37
maoap1 <maoap1@gmail.com>
2017-12-03 11:56:22 +0100

    Clouds and baloon

--- LD40/Assets/Scripts/BalloonController.cs
+++ LD40/Assets/Scripts/BalloonController.cs
@@ -15,12 +15,13 @@ public class BalloonController : MonoBehaviour {
     public float fallingObjectThrow;
     public float fallingObjectTorque;
 
-    public int health = 100;
-    public int goldBars = 0;
-
+	public int health;
+	public int goldBars;
     public float goldBarMass; 
 	public float hSpeed;
 	public float vSpeed;
+	public float velocityImpact;
+	public int birdImpact;
 
     private bool xWasDown = false;
     private Rigidbody2D rb;
@@ -28,58 +29,67 @@ public class BalloonController : MonoBehaviour {
 	private Rigidbody2D[] rbs;
 	private PolygonCollider2D[] pcols;
 
+	private bool dead;
+
+
     // Use this for initialization
     void Start () {
-		rb = GetComponent<Rigidbody2D>();
-		pcol = GetComponent<PolygonCollider2D> ();
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		pcol = gameObject.GetComponent<PolygonCollider2D> ();
 		rbs = gameObject.GetComponentsInChildren<Rigidbody2D> ();
 		pcols = gameObject.GetComponentsInChildren<PolygonCollider2D>();
 		rb.mass = 1.0f;
+		dead = false;
 	}
 
-	// Update is called once per frame
-	void Update () {
-        hpCountText.text = "" + health;
-        goldBarCountText.text = "" + goldBars;
-		float hAxis = Input.GetAxis("Horizontal");
-		float burn = Input.GetAxis("Burn");
-        bool xDown = Input.GetKey(KeyCode.X);
 
-        float maxVForce = vSpeed;
-        float maxHForce = hSpeed * hAxis;
-
-		if (burn > 0)
+	void FixedUpdate () {
+		if (!dead) 
 		{
-			rb.AddForce(new Vector2(0.0f, 1.0f) * maxVForce);
+			hpCountText.text = "" + health;
+			goldBarCountText.text = "" + goldBars;
+
+			float hAxis = Input.GetAxis("Horizontal");
+			float burn = Input.GetAxis("Burn");
+			bool xDown = Input.GetKey(KeyCode.X);
+
+			float maxVForce = vSpeed;
+			float maxHForce = hSpeed * hAxis;
+
+			if (burn > 0)
+			{
+				rb.AddForce(new Vector2(0.0f, 1.0f) * maxVForce);
+			}
+			rb.AddForce(new Vector2(1.0f, 0.0f) * maxHForce);
+
+			if (xDown && xWasDown == false)
+			{
+				xWasDown = true;
+				if (goldBars > 0)
+				{
+					goldBars--;
+					rb.mass -= goldBarMass;
+					Transform fallingInstance = Instantiate(fallingObject, fallingObjectSpawnLocation.position, Quaternion.identity);
+					Rigidbody2D fRB = fallingInstance.GetComponent<Rigidbody2D>();
+					if (fRB != null)
+					{
+						fRB.AddForce(new Vector2(-fallingObjectThrow, 0.0f));
+						fRB.AddTorque(fallingObjectTorque);
+					}
+				}
+			}
+
+			if (xDown == false)
+				xWasDown = false;
+			/*
+			if (health <= 0)
+			{
+				Instantiate(explosionEffect, transform.position, Quaternion.identity);
+				Death();
+			}
+			*/
 		}
-		rb.AddForce(new Vector2(1.0f, 0.0f) * maxHForce);
-
-        if (xDown && xWasDown == false)
-        {
-            xWasDown = true;
-            if (goldBars > 0)
-            {
-                goldBars--;
-				rb.mass -= goldBarMass;
-                Transform fallingInstance = Instantiate(fallingObject, fallingObjectSpawnLocation.position, Quaternion.identity);
-                Rigidbody2D fRB = fallingInstance.GetComponent<Rigidbody2D>();
-                if (fRB != null)
-                {
-                    fRB.AddForce(new Vector2(-fallingObjectThrow, 0.0f));
-                    fRB.AddTorque(fallingObjectTorque);
-                }
-            }
-        }
         
-        if (xDown == false)
-            xWasDown = false;
-
-        if (health == 0)
-        {
-            Instantiate(explosionEffect, transform.position, Quaternion.identity);
-			Death();
-			// do not forget to make health 100 once again
-        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -102,24 +112,42 @@ public class BalloonController : MonoBehaviour {
         }
         else if (other.gameObject.tag == "Mountain")
         {
-            health -= 20;
+			float velocity = other.relativeVelocity.magnitude;
+			float f;
+			if (velocity < 1.6f) {
+				f = 0.3f * velocity;
+			} else if (velocity < 3.5f) {
+				f = 0.7f * velocity;
+			} else if (velocity < 5.0f) {
+				f = 1.0f * velocity;
+			} else if (velocity < 10.0f) {
+				f = 1.5f * velocity;
+			} else {
+				f = 2.0f * velocity;
+			}
+
+			health -= (int)(velocityImpact*f);
         }
         else if (other.gameObject.tag == "Bird")
         {
-            health -= 10;
+            health -= birdImpact;
         }
     }
 
 	void Death()
 	{
-		rb.simulated = false;
-		pcol.enabled = false;
+		Destroy(rb);
+		Destroy (pcol);
+
 		foreach (var item in pcols) {
 			item.enabled = true;			
 		}
 		foreach (var item in rbs) {
 			item.simulated = true;
 		}
+
+		dead = true;
+
 	}
 		
 }
