7c0194d8911faad3e38687d599ecb4b4d6a6523e
Jeysym <bryda.jan@gmail.com>
2017-12-03 19:41:26 +0100

    Improved UI + exit button

---
+++ LD40/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour {
+
+    public void QuitApplication()
+    {
+        Debug.Log("Application quits now...");
+        Application.Quit();
+    }
+}
